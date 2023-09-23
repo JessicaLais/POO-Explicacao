a243cf19baca3e8e6a57fe4ad44f2557a6497752
JessicaLais <142358374+JessicaLais@users.noreply.github.com>
2023-09-22 21:35:47 -0300

    Executavel.java

--- heranca/Executavel.java
+++ heranca/Executavel.java
@@ -1,13 +1,18 @@
-package heranca.empresa.Executavel;
+package heranca;
 
+import heranca.empresa.Diretor;
 import heranca.empresa.Engenheiro;
+import heranca.empresa.Gerente;
 
 public class Executavel{
 
     public static void main(String[] args){
        
-        Engenheiro eng1 = new Engenheiro(nome :"Jota", cpf:123,telefone:801, crea:"123");
-        system.out.println(eng1);
-
+        Engenheiro eng1 = new Engenheiro("Jota",057,123,"456","opa");
+        System.out.println(eng1);
+        Gerente ger1 = new Gerente("Agar", 111, 33337778, 1);
+        System.out.println(ger1);
+        Diretor dir1 = new Diretor("Ata", 875, 99998888,10);
+        System.out.println(dir1);
     }
 }
