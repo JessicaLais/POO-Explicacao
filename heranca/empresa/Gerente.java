aa362d0819fef1c62431f83d2de93af3ae05acbd
JessicaLais <142358374+JessicaLais@users.noreply.github.com>
2023-09-22 21:40:55 -0300

    Update Gerente.java

--- heranca/empresa/Gerente.java
+++ heranca/empresa/Gerente.java
@@ -3,60 +3,34 @@ package heranca.empresa;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-public class Gerente{
+public class Gerente extends Funcionario{
 
-    private String nome;
-    private Integer cpf;
-    private Integer telefone;
     private Integer quantidadeGerenciados;
 
     public Gerente(){
     }
 
     public Gerente(String nome, Integer cpf, Integer telefone, Integer quantidadeGerenciados){
-        this.nome = nome;
-        this.cpf = cpf;
-        this.telefone = telefone;
+        super(nome, cpf, telefone);
         this.quantidadeGerenciados = quantidadeGerenciados;
-    }
-
-    public String getNome(){
-        return this.nome;
-    }
-
-    public void setNome(String nome){
-        this.nome = nome;
-    }
-
-    public Integer getCpf(){
-        return this.cpf;
-    }
-
-    public void setCpf(Integer cpf){
-        this.cpf = cpf;
-    }
-    public Integer getTelefone(){
-        return this.cpf;
-    }
-
-    public void setTelefone(Integer telefone){
-        this.telefone = telefone;
     }
 
     public Integer getquatidadeGerenciados(){
         return this.quantidadeGerenciados;
     }
 
-    public void setquantidadeGerenciado(String quantidadeGerenciados){
+    public void setquantidadeGerenciado(Integer quantidadeGerenciados){
         this.quantidadeGerenciados = quantidadeGerenciados;
     }
-
+    
+    @Override
     public BigDecimal getSalario(){
-        return new BigDecimal(val:20000.00);
+        return new BigDecimal(20000.00);
     }
-
+    
+    @Override
     public BigDecimal getBonificacao(){
-        return getSalario().multiply(new BigDecimal(val:0.35));
+        return getSalario().multiply(new BigDecimal(0.35));
 
     }
     @Override
